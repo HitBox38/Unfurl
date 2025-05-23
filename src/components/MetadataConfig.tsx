cd18c207107a1efa99263bc74cc9b24d126f4c43
Tomer Norman <spidernor38@gmail.com>
2025-05-23 21:37:51 +0300

    Added new tooltip explaining metadata config

--- src/components/MetadataConfig.tsx
+++ src/components/MetadataConfig.tsx
@@ -1,14 +1,23 @@
-import { Button } from "@mui/material";
+import { Box, Button, Tooltip } from "@mui/material";
 import { useDialogStore } from "../stores/DialogStore";
 import { useMetadataConfigFormModal } from "../modals/MetadataConfigFormModal";
+import { Help } from "@mui/icons-material";
 
 export const MetadataConfig = () => {
   const { setContent } = useDialogStore((state) => state);
 
   const content = useMetadataConfigFormModal();
   return (
-    <Button variant="contained" onClick={() => setContent(content)}>
-      Config Metadata
-    </Button>
+    <Box display="flex" flexDirection="row" gap="10px" alignItems="center">
+      <Button variant="contained" onClick={() => setContent(content)}>
+        Config Metadata
+      </Button>
+      <Tooltip
+        arrow
+        placement="top"
+        title="Define custom data fields (like player stats, story flags, or game variables) that can be parsed from your story files using special symbols and edited in the node editor.">
+        <Help />
+      </Tooltip>
+    </Box>
   );
 };
